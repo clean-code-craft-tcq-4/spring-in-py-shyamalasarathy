36f2716d25f993fa967324db96b25fac2cc46ea1
Shyamala Sarathy <105268170+shyamalasarathy@users.noreply.github.com>
2022-05-21 16:24:09 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -4,4 +4,4 @@ def calculateStats(numbers):
   if(len(numbers) == 0):
     return {"avg" : "nan", "max" : "nan", "min" : "nan"}
   else:
-  return{"avg" = sum(numbers)/len(numners), "max" = max(numbers), "min" = min(numbers)}
+    return{"avg" = sum(numbers)/len(numners), "max" = max(numbers), "min" = min(numbers)}
